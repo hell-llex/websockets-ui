c0e32dfcde04c671c7ddde790d548d0638a2de78
Alexander Demeshchenko <demeshenko333@gmail.com>
2024-10-28 18:20:00 +0300

    feat: fix server and add router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,39 @@
+// import { WebSocket } from 'ws';
 import {
+	addShipsController,
+	addUserToRoomController,
+	attackController,
+	createRoomController,
 	createUserController,
 	// validateUserController
 } from '../controller';
+import { getGameRoomByIdStore } from '../store/gameStore';
 import { WebSocketMessage } from '../types';
 
-export const router = async (data: WebSocketMessage) => {
-	console.log('data :>> ', data);
+export const router = async (clientId: string, data: WebSocketMessage) => {
+	// console.log('data :>> ', data);
 
 	switch (data.type) {
 		case 'reg':
-			return await createUserController(data);
+			return await createUserController(clientId, data);
 			break;
-		case 'reg':
-			return await createUserController(data);
+		case 'create_room':
+			return await createRoomController(clientId, data);
+			break;
+		case 'add_user_to_room':
+			return await addUserToRoomController(clientId, data);
+			break;
+		case 'add_ships':
+			return await addShipsController(clientId, data);
+			break;
+		case 'attack':
+			const { gameId, indexPlayer } = JSON.parse(data.data);
+			const currentGame = await getGameRoomByIdStore(gameId);
+			if (currentGame && currentGame.currentPlayer === indexPlayer)
+				return await attackController(clientId, data);
+			break;
+		case 'randomAttack':
+			return await attackController(clientId, data); // TODO: добавить позицию
 			break;
 		default:
 			break;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import { WebSocketServer, WebSocket } from 'ws';
 import { WebSocketMessage } from './types';
 import { router } from './router';
-import { getUsersStore } from './store/userStore';
+import { addClientStore, deleteClientStore } from './store/clientStore';
+import { generateId } from './utils';
 
 const PORT = 3000;
 
@@ -10,46 +11,19 @@ const wss = new WebSocketServer({ port: PORT });
 
 console.log(`WebSocket server is running on ws://localhost:${PORT}`);
 
-// Хранение клиентов
-// const clients: Set<WebSocket> = new Set();
-
 wss.on('connection', async (ws: WebSocket) => {
-	ws.on('message', async (message) => {
-		console.log('message :>> ', message);
-		const data: WebSocketMessage = JSON.parse(message.toString());
-
-		const res = await router(data);
-
-		console.log('await getUsersStore(); :>> ', await getUsersStore());
-		console.log('---------\nres :>> ', res);
-
-		ws.send(JSON.stringify(res));
-	});
-
+	const clientId = generateId();
+	addClientStore(clientId, ws);
 	console.log('Client connected');
 
-	// Добавляем клиента в набор
-	// clients.add(ws);
+	ws.on('message', async (message) => {
+		const data: WebSocketMessage = JSON.parse(message.toString());
+		console.log(`Получение сообщения ${data.type} от ${clientId}`);
+		await router(clientId, data);
+	});
 
-	// Обработка входящих сообщений
-	// ws.on('message', async (req: string) => {
-	// 	const message = await JSON.parse(req);
-	// 	console.log(`Received: `, await JSON.parse(message.data));
-	// 	// Здесь можно обрабатывать сообщения и отправлять ответы
-	// });
-
-	// Обработка отключения клиента
 	ws.on('close', () => {
 		console.log('Client disconnected');
-		// clients.delete(ws);
+		deleteClientStore(clientId);
 	});
 });
-
-// Функция для отправки сообщения всем подключенным клиентам
-// function broadcast(message: string) {
-// 	clients.forEach((client) => {
-// 		if (client.readyState === WebSocket.OPEN) {
-// 			client.send(message);
-// 		}
-// 	});
-// }
