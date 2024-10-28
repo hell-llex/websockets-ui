5c7068d96e2711bb49e8a69d8b5f43f318fab90d
Alexander Demeshchenko <demeshenko333@gmail.com>
2024-10-28 18:30:00 +0300

    feat: add types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,8 @@
-// types.ts
 export interface User {
-	id: string;
+	clientId: string;
 	name: string;
 	password: string;
+	wins: number; // count of wins
 }
 
 export interface CreateUserPayload {
@@ -17,6 +17,44 @@ export interface ValidateUserPayload {
 
 export interface WebSocketMessage {
 	type: string;
-	data?: any; // Можно сделать более строгим, если нужно
+	data?: any;
 	id: number;
+}
+
+export type RoomUser = {
+	name: string;
+	index: string;
+};
+
+export interface Ship {
+	position: {
+		x: number;
+		y: number;
+	};
+	hitPositions?: any;
+	direction: boolean;
+	length: number;
+	type: 'small' | 'medium' | 'large' | 'huge';
+}
+
+export interface Players {
+	clientId: string;
+	name: string;
+	idPlayer: string;
+	ships?: Ship[];
+}
+
+export type UserYourTurn = 0 | 1;
+
+export interface GameRoom {
+	idGame: string;
+	roomUsers: Players[];
+	roomId: string;
+	userYourTurn: UserYourTurn;
+	currentPlayer: string;
+}
+
+export interface Room {
+	roomId: string;
+	roomUsers: RoomUser[];
 }
