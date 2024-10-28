cee2d8673ecf3e69578c208af31e52ae88ea6d39
Alexander Demeshchenko <demeshenko333@gmail.com>
2024-10-28 19:00:00 +0300

    feat: add controller and service

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,13 +1,112 @@
 import { WebSocketMessage } from '../types';
-// import { addUserStore, getUsersStore } from './store/userStore';
-import { createUserService, validateUserService } from '../service';
+import {
+	addUserToRoomService,
+	attackService,
+	checkForStartToGameRoomService,
+	createGameService,
+	createRoomService,
+	createUserService,
+	turnUserService,
+	updateRoomService,
+	updateShipsService,
+	updateWinnersService,
+} from '../service';
+import { unicast } from '../utils';
+import { broadcast } from '../utils';
+import { getClientByIdStore, getClientsStore } from '../store/clientStore';
 
-export const createUserController = async (data: WebSocketMessage) => {
-	const res = await createUserService(data);
-	return res;
+export const createUserController = async (clientId: string, data: WebSocketMessage) => {
+	unicast(await getClientByIdStore(clientId), await createUserService(clientId, data));
+	broadcast(await getClientsStore(), await updateRoomService());
+	broadcast(await getClientsStore(), await updateWinnersService());
 };
 
-export const validateUserController = async (data: WebSocketMessage) => {
-	const res = await validateUserService(data);
-	return res;
+export const createRoomController = async (clientId: string, _data?: WebSocketMessage) => {
+	await createRoomService(clientId);
+	broadcast(await getClientsStore(), await updateRoomService());
+};
+
+export const addUserToRoomController = async (clientId: string, data: WebSocketMessage) => {
+	const roomToGameId = await addUserToRoomService(clientId, data);
+	if (roomToGameId) {
+		broadcast(await getClientsStore(), await updateRoomService());
+		const game = await createGameService(roomToGameId);
+		if (game) {
+			for (let i = 0; i < game.roomUsers.length; i++) {
+				const roomUser = game.roomUsers[i];
+				unicast(await getClientByIdStore(roomUser.clientId), {
+					type: 'create_game',
+					data: {
+						idGame: game.idGame,
+						idPlayer: roomUser.idPlayer,
+					},
+					id: 0,
+				});
+			}
+		}
+	}
+};
+
+export const addShipsController = async (_clientId: string, data: WebSocketMessage) => {
+	await updateShipsService(data);
+
+	const checkGameRoom = await checkForStartToGameRoomService(data);
+
+	if (checkGameRoom && checkGameRoom.isStartsGame) {
+		const game = checkGameRoom.currentGame;
+		for (let i = 0; i < game.roomUsers.length; i++) {
+			const roomUser = game.roomUsers[i];
+			unicast(await getClientByIdStore(roomUser.clientId), {
+				type: 'start_game',
+				data: {
+					ships: roomUser.ships,
+					currentPlayerIndex: roomUser.idPlayer,
+				},
+				id: 0,
+			});
+		}
+		game.roomUsers.forEach(async (roomUser) =>
+			unicast(await getClientByIdStore(roomUser.clientId), {
+				type: 'turn',
+				data: {
+					currentPlayer: await turnUserService(game),
+				},
+				id: 0,
+			})
+		);
+	}
+};
+
+export const attackController = async (clientId: string, req: WebSocketMessage) => {
+	const res = await attackService(clientId, req);
+	const { game, clients, data, sendPosition } = res!;
+	const { status, currentPlayer } = data;
+
+	const determineCurrentPlayer = async (status: string, currentPlayer: string) => {
+		return status === 'killed' || status === 'shot' ? currentPlayer : await turnUserService(game);
+	};
+
+	for (let i = 0; i < sendPosition.length; i++) {
+		data.position = sendPosition[i];
+		data.status = sendPosition.length === 1 || !i ? status : 'miss';
+
+		await broadcast(clients, {
+			type: 'attack',
+			data: data,
+			id: 0,
+		});
+		clients.forEach(async (client) =>
+			unicast(client, {
+				// TODO: исправить бак когда умирает корабль срабатывает дважды
+				type: 'turn',
+				data: {
+					currentPlayer: await determineCurrentPlayer(
+						sendPosition.length === 1 ? status : 'shot',
+						currentPlayer
+					),
+				},
+				id: 0,
+			})
+		);
+	}
 };
--- src/service/index.ts
+++ src/service/index.ts
@@ -1,13 +1,28 @@
-import { User, WebSocketMessage } from '../types';
-import { addUserStore, getUsersStore } from '../store/userStore';
+import { GameRoom, Ship, User, UserYourTurn, WebSocketMessage } from '../types';
+import { addUserStore, getUserByIdStore, getUsersStore } from '../store/userStore';
+import {
+	addUserToRoomStore,
+	createRoomStore,
+	getRoomByIdStore,
+	getRoomsStore,
+} from '../store/roomStore';
+import { generateId } from '../utils';
+import {
+	addGameRoomStore,
+	createTurnManagerStore,
+	getGameRoomByIdStore,
+	updateShipsToGameRoomStore,
+} from '../store/gameStore';
+import { getClientByIdStore } from '../store/clientStore';
+import { WebSocket } from 'ws';
 
-export const createUserService = async (req: WebSocketMessage) => {
+export const createUserService = async (clientId: string, req: WebSocketMessage) => {
 	const reqParse = { ...req, data: JSON.parse(req.data) };
 	const res = {
 		type: 'reg',
 		data: {
 			name: reqParse.data.name,
-			index: 0,
+			index: clientId,
 			error: false,
 			errorText: '',
 		},
@@ -17,37 +32,216 @@ export const createUserService = async (req: WebSocketMessage) => {
 		const users = await getUsersStore();
 		const userExists = users.some((user: User) => user.name === reqParse.data.name);
 		if (userExists) {
-			// логиним
-			// await addUserStore({ id: reqParse.id, ...reqParse.data });
 		} else {
-			// создаем
-			await addUserStore({ id: reqParse.id, ...reqParse.data });
+			await addUserStore({ clientId: clientId, ...reqParse.data, wins: 0 });
 		}
 
-		return { ...res, data: JSON.stringify({ ...res.data, index: users.length }) };
+		return res;
 	} catch (error: any) {
 		return {
 			...res,
-			data: JSON.stringify({
+			data: {
 				name: res.data.name,
-				index: 0,
+				index: clientId,
 				error: true,
 				errorText: error.message,
-			}),
+			},
 		};
 	}
 };
 
-export const validateUserService = async (req: WebSocketMessage) => {
-	return req;
-	// const users = await getUsersStore();
-	// const existingUser = users.find(
-	// 	(user) => user.name === payload.name && user.password === payload.password
-	// );
+export const createRoomService = async (clientId: string) => {
+	const user = await getUserByIdStore(clientId);
+	if (user) await createRoomStore({ name: user.name, index: user.clientId });
+};
 
-	// if (existingUser) {
-	// 	return { type: 'success', message: 'Пользователь валиден' };
-	// } else {
-	// 	return { type: 'error', message: 'Неправильные данные пользователя' };
-	// }
+export const updateWinnersService = async () => {
+	const winners = (await getUsersStore()).filter((winner) => winner.wins !== 0);
+	return {
+		type: 'update_winners',
+		data: winners,
+		id: 0,
+	};
+};
+
+export const updateRoomService = async () => {
+	const rooms = await getRoomsStore();
+	const validRooms = rooms.filter((room) => room.roomUsers.length <= 1);
+	return {
+		type: 'update_room',
+		data: validRooms,
+		id: 0,
+	};
+};
+
+export const addUserToRoomService = async (clientId: string, req: WebSocketMessage) => {
+	const reqParse = { ...req, data: JSON.parse(req.data) };
+	const user = await getUserByIdStore(clientId);
+	if (user)
+		return await addUserToRoomStore(reqParse.data.indexRoom, {
+			name: user.name,
+			index: user.clientId,
+		});
+};
+
+export const createGameService = async (roomId: string) => {
+	const room = await getRoomByIdStore(roomId);
+	if (room) {
+		const gameRoom = {
+			idGame: generateId(),
+			roomId: roomId,
+			userYourTurn: 1 as UserYourTurn,
+			currentPlayer: '',
+			roomUsers: room.roomUsers.map((roomUser) => {
+				return {
+					idPlayer: generateId(),
+					name: roomUser.name,
+					clientId: roomUser.index,
+				};
+			}),
+		};
+		gameRoom.currentPlayer = gameRoom.roomUsers[0].idPlayer;
+		await addGameRoomStore(gameRoom.idGame, gameRoom);
+		return gameRoom;
+	}
+};
+
+export const updateShipsService = async (req: WebSocketMessage) => {
+	const reqParse = { ...req, data: JSON.parse(req.data) };
+	const [gameId, ships, indexPlayer] = [
+		reqParse.data.gameId,
+		reqParse.data.ships,
+		reqParse.data.indexPlayer,
+	];
+	await updateShipsToGameRoomStore(gameId, ships, indexPlayer);
+};
+
+export const checkForStartToGameRoomService = async (req: WebSocketMessage) => {
+	const reqParse = { ...req, data: JSON.parse(req.data) }; // TODO: перенести этот двойной парсер в корень и вынести в utils
+	const currentGame = await getGameRoomByIdStore(reqParse.data.gameId);
+	if (currentGame) {
+		return {
+			isStartsGame: currentGame.roomUsers.every(
+				(roomUser) => roomUser.ships && roomUser.ships?.length !== 0
+			),
+			currentGame: currentGame,
+		};
+	}
+};
+
+const turnManagerStore = createTurnManagerStore();
+
+export const turnUserService = async (game: GameRoom) => {
+	const currentPlayerId = await turnManagerStore(game);
+	return currentPlayerId;
+};
+
+const getSurroundingCells = (ship: Ship) => {
+	const surroundingCells: { x: number; y: number }[] = [];
+	const { position, direction, length } = ship;
+
+	const occupiedPositions: { x: number; y: number }[] = [];
+	for (let i = 0; i < length; i++) {
+		const shipX = direction ? position.x : position.x + i;
+		const shipY = direction ? position.y + i : position.y;
+		occupiedPositions.push({ x: shipX, y: shipY });
+	}
+
+	for (let i = 0; i < length; i++) {
+		const shipX = direction ? position.x : position.x + i;
+		const shipY = direction ? position.y + i : position.y;
+
+		const potentialSurroundings = [
+			{ x: shipX - 1, y: shipY - 1 },
+			{ x: shipX - 1, y: shipY },
+			{ x: shipX - 1, y: shipY + 1 },
+			{ x: shipX, y: shipY - 1 },
+			{ x: shipX, y: shipY + 1 },
+			{ x: shipX + 1, y: shipY - 1 },
+			{ x: shipX + 1, y: shipY },
+			{ x: shipX + 1, y: shipY + 1 },
+		];
+
+		potentialSurroundings.forEach((cell) => {
+			const isOccupied = occupiedPositions.some((pos) => pos.x === cell.x && pos.y === cell.y);
+			const isHitPosition = ship.hitPositions?.some(
+				(hitPos: { x: number; y: number }) => hitPos.x === cell.x && hitPos.y === cell.y
+			);
+
+			if (!isOccupied && !isHitPosition) {
+				if (
+					!surroundingCells.some(
+						(surroundingCell) => surroundingCell.x === cell.x && surroundingCell.y === cell.y
+					)
+				) {
+					surroundingCells.push(cell);
+				}
+			}
+		});
+	}
+
+	return surroundingCells;
+};
+
+export const attackService = async (_clientId: string, req: WebSocketMessage) => {
+	const reqParse = { ...req, data: JSON.parse(req.data) };
+	const { gameId, x, y, indexPlayer } = reqParse.data;
+	const attackPosition = { x, y };
+
+	const game = await getGameRoomByIdStore(gameId);
+	const attackerId = indexPlayer;
+
+	if (game) {
+		const opponent = game.roomUsers.find((user) => user.idPlayer !== attackerId);
+		if (!opponent) {
+			throw new Error('Opponent not found');
+		}
+
+		let status: 'miss' | 'killed' | 'shot' = 'miss';
+		let sendPosition: { x: number; y: number }[] = [];
+
+		for (const ship of opponent.ships!) {
+			const occupiedPositions = [];
+			for (let i = 0; i < ship.length; i++) {
+				const pos = ship.direction
+					? { x: ship.position.x, y: ship.position.y + i } // Вертикально
+					: { x: ship.position.x + i, y: ship.position.y }; // Горизонтально
+				occupiedPositions.push(pos);
+			}
+
+			const positionHit = occupiedPositions.find(
+				(pos) => pos.x === attackPosition.x && pos.y === attackPosition.y
+			);
+
+			if (positionHit) {
+				if (!('hitPositions' in ship)) {
+					ship.hitPositions = [];
+				}
+
+				ship.hitPositions.push(attackPosition);
+
+				status = ship.hitPositions.length === ship.length ? 'killed' : 'shot';
+				sendPosition =
+					status === 'killed' ? [attackPosition, ...getSurroundingCells(ship)] : [attackPosition];
+				break;
+			}
+		}
+
+		const clients = (
+			await Promise.all(game.roomUsers.map((user) => getClientByIdStore(user.clientId)))
+		).flat() as WebSocket[];
+
+		if (status === 'miss') sendPosition = [attackPosition];
+
+		return {
+			game,
+			clients,
+			data: {
+				position: attackPosition,
+				currentPlayer: attackerId,
+				status,
+			},
+			sendPosition,
+		};
+	}
 };
